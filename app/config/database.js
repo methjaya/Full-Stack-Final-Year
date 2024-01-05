e11476de1dbc66e078148fa156ca223bcf543407
Sahansa Jayawardhana <79324471+methjaya@users.noreply.github.com>
2024-01-05 22:31:24 +0530

    DB String added

--- app/config/database.js
+++ app/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const DBConnect = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/gymdb');
+    const conn = await mongoose.connect('mongodb+srv://methjaya252:VJh6SYmym3wIbIfn@cluster0.zjgxoen.mongodb.net/?retryWrites=true&w=majority');
 
     console.log(`MongoDB Connected: ${conn.connection.host}`)
     
