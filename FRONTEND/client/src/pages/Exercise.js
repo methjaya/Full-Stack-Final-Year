e2e8a2c4d9f399842621bdaff1c23aeaa0f3ae30
Sahansa Jayawardhana <79324471+methjaya@users.noreply.github.com>
2024-01-05 09:18:57 +0530

    adding Workout CRUD page

--- FRONTEND/client/src/pages/Exercise.js
+++ FRONTEND/client/src/pages/Exercise.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import Auth from "../utils/auth";
 import Header from "../components/Header";
 import cardioIcon from "../assets/images/cardio.png"
 import resistanceIcon from "../assets/images/resistance.png"
-
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 export default function Exercise() {
   const loggedIn = Auth.loggedIn();
-  const navigate = useNavigate()
-
+  const navigate = useNavigate();
 
   // If the user is not logged in, redirect to the login page
   if (!loggedIn) {
@@ -20,22 +19,39 @@ export default function Exercise() {
     <div>
       <Header />
       <div className="exercise d-flex flex-column align-items-center">
-        <h2 className='title'>Add Exercise!!!</h2>
-        <div>
-          <button className='cardio-btn d-flex flex-column  align-items-center justify-content-center' onClick={() => navigate("/exercise/cardio")}>
-            <img alt="cardio" src={cardioIcon} className="exercise-icon" />
-            Cardio
-          </button>
+        <h2 className='title'>Make Your Workout with Us!</h2>
+        <div className="row">
+
+        
+          <div className="col-md-6">
+            <button className='cardio-btn d-flex flex-column align-items-center justify-content-center' onClick={() => navigate("/exercise/cardio")}>
+              <img alt="cardio" src={cardioIcon} className="exercise-icon" />
+              Cardio
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button className='resistance-btn d-flex flex-column align-items-center justify-content-center' onClick={() => navigate("/exercise/resistance")}>
+              <img alt="resistance" src={resistanceIcon} className="exercise-icon" />
+              Resistance
+            </button>
+          </div>
         </div>
-        <div>
-          <button className='resistance-btn d-flex flex-column  align-items-center justify-content-center' onClick={() => navigate("/exercise/resistance")}>
-            <img alt="resistance" src={resistanceIcon} className="exercise-icon" />
-            Resistance
-          </button>
+
+        <div className="row">
+          <div className="col-md-6">
+            <button className='abs-btn d-flex flex-column align-items-center justify-content-center' onClick={() => navigate("/exercise/cardio")}>
+              <img alt="cardio" src={cardioIcon} className="exercise-icon" />
+              Abs
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button className='track-btn d-flex flex-column align-items-center justify-content-center' onClick={() => navigate("/exercise/resistance")}>
+              <img alt="rtrack" src={resistanceIcon} className="exercise-icon" />
+              Track
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-
