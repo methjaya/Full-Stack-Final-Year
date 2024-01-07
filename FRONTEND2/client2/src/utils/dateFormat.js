92f133b7f8d74a005384488dcd072bda845ad451
Sahansa Jayawardhana <79324471+methjaya@users.noreply.github.com>
2024-01-07 16:03:28 +0530

    FRONTEND2 Folder removed

--- FRONTEND2/client2/src/utils/dateFormat.js
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date) => {
-    const options = {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit"
-    }
-    return (new Date(date)).toLocaleDateString("en-US", options);
-} 
